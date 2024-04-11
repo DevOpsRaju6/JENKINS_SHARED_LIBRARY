6fcc111b2f6e0e3f8a8fc0a6c833c21b6c9bed2a
aravind <aravindnutenky@example.com>
2024-04-11 20:29:33 +0530

    modified yotube to youtube1

--- vars/runcontainer.groovy
+++ vars/runcontainer.groovy
@@ -1,3 +1,3 @@
 def call() {
-    sh 'docker run -d --name youtube -p 3000:3000 devopsraju6/youtube:latest'
+    sh 'docker run -d --name youtube1 -p 3000:3000 devopsraju6/youtube1:latest'
 }
--- vars/trivyimagescan.groovy
+++ vars/trivyimagescan.groovy
@@ -1,3 +1,3 @@
 def call() {
-    sh 'trivy image devopsraju6/youtube:latest > trivyimage.txt'
+    sh 'trivy image devopsraju6/youtube1:latest > trivyimage.txt'
 }
